31a813a6f9caaa4f45d9dd7421d82aee60ffe2dd
DenverTheDino <christophe.dwwm48@gmail.com>
2024-02-12 16:35:59 +0100

    debut header 16h34 12/02

---
+++ themes/greta_theme_berg/apropos.php
@@ -0,0 +1,8 @@
+
+<?php
+/* 
+    Template Name: A propos
+    Template Post Type: formation, page
+*/
+    include('header.php');
+    ?>
--- themes/greta_theme_berg/header.php
+++ themes/greta_theme_berg/header.php
@@ -4,11 +4,50 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Mina&display=swap" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" type="text/css" /> 
 </head>
 <body>
-<header>
- <h1 class="text-center">Greta Gard Lozere</h1>
+<header class="w-100">
+<div class="row">
+    <div class="col-sm">
+      <img src="<?php echo bloginfo('template_directory')."/ressources/greta.png"?>" alt="occitanie">
+    </div>
+    <div class="col-sm d-flex justify-content-end">
+            <nav class=" navbar navbar-expand-lg">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="">Page d'accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Actualités</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo bloginfo('url')."/apropos.php"?>">A Propos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Formations</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Contact</a>
+                </li>      
+                </ul>
+            </div>
+        </nav>
+    </div>
+  </div>
+  
+
+
 </header>
 
