2ab97f7e03806e38c8ee79620fc9fa6b6930c2e0
DenverTheDino <christophe.dwwm48@gmail.com>
2024-02-13 13:57:44 +0100

    footer finito denver 13:57 13/02

--- themes/greta_theme_berg/footer.php
+++ themes/greta_theme_berg/footer.php
@@ -1,7 +1,22 @@
 
-
-<footer>
-
+<footer class="bg-body-tertiary text-center text-lg-start">
+  <!-- Copyright -->
+<div class="text-center p-3"">
+    © 2024 Copyright By Greta Theme_berg
+</div>
+  <!-- Copyright -->
+  
+<div class="fr-col-12 fr-col-sm-6 fr-col-md-3 text-center">
+    <ul>
+        <li>
+            <a href="#" target="_blank">Mentions légales</a>
+        </li>
+        <li>
+            <a href='#' target="_blank">Politiques de confidentialité</a>
+        </li>
+    </ul>
+</div>    
 </footer>
+
 </body>
 </html>
