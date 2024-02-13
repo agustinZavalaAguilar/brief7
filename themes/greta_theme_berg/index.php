64fce48f649a7dddd5ef8ea3c55a97aaa506725c
DenverTheDino <christophe.dwwm48@gmail.com>
2024-02-13 14:13:22 +0100

    rajout hero banner

--- themes/greta_theme_berg/index.php
+++ themes/greta_theme_berg/index.php
@@ -1,6 +1,19 @@
 <?php
 include('header.php')
 ?>
+<!-- Section herobanner -->
+<section class="hero_banner ">
+    <img src="<?php echo bloginfo('template_directory')."/images/hero-banner.png"?>" alt="">
+</section>
+<!-- Section a propos -->
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col">A propos</div>
+        </div>
+    </div>
+</section>
+
 <?php
 include('footer.php')
 ?>
