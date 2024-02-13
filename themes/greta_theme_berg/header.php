c8883dace09a56d84c9ffd22beeb20ea28a4f8a5
DenverTheDino <christophe.dwwm48@gmail.com>
2024-02-13 10:16:59 +0100

    header terminer 13/02 10h16 Denver
    juste a link les différentes pages

--- themes/greta_theme_berg/header.php
+++ themes/greta_theme_berg/header.php
@@ -11,15 +11,15 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" type="text/css" /> 
+    <link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" type="text/css" />
 </head>
 <body>
 <header class="w-100">
 <div class="row">
-    <div class="col-sm">
+    <div class="col-sm w-50">
       <img src="<?php echo bloginfo('template_directory')."/ressources/greta.png"?>" alt="occitanie">
     </div>
-    <div class="col-sm d-flex justify-content-end">
+    <div class="col-sm w-50 d-flex justify-content-end">
             <nav class=" navbar navbar-expand-lg">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
