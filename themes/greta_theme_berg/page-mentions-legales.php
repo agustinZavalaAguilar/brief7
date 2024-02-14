5a97a23d5120ea85100bd994c19d6cca15f10c09
agustinZavalaAguilar <88766456+agustinZavalaAguilar@users.noreply.github.com>
2024-02-14 17:29:45 +0100

    mentions legales

---
+++ themes/greta_theme_berg/page-mentions-legales.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php include ('header.php'); ?>
+
+    <div class="container mentions-legales">
+        <div class ="row" >
+            <div class= "col col-12 w-100 mb-5">
+            <?php  the_post_thumbnail(); ?>
+            </div>
+            <div class="col width-12">
+                <?php the_content(); ?>
+            </div>
+        </div>
+    </div>
+
+    <?php include ('footer.php'); ?>
+
+</body>
+</html>
