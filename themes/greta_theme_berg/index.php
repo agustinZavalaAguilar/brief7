51aaa8c2aee4b8a47d41a1ea6ec6e7504abca244
DenverTheDino <christophe.dwwm48@gmail.com>
2024-02-14 16:38:24 +0100

    fin slider et card et finaliser partner 16:16 14/02 denver

--- themes/greta_theme_berg/index.php
+++ themes/greta_theme_berg/index.php
@@ -4,7 +4,7 @@ include('header.php')
 <!-- Section herobanner -->
 <section class="hero_banner">   
     <h1 class="text-center text-uppercase">Formez vous avec le greta</h1>
-    <a class="d-flex justify-content-center" href=""><button class="btn btn-primary" type="button">CONTACT</button></a>
+    <a class="d-flex justify-content-center" href=""><button class="btn" type="button">CONTACT</button></a>
 </section>
     <!-- section a propos -->
 <section class="propos">
@@ -31,13 +31,13 @@ include('header.php')
   <div id="carouselExampleControls" class="carousel slide w-50 d-flex justify-content-center" data-bs-ride="carousel">
   <div class="carousel-inner text-align">
     <div class="carousel-item active">
-      <a href=""><img src=<?php echo bloginfo('template_directory')."/images/dev-web.png"?> class="d-block w-100" alt="..."></a>
+      <a href=""><img src=<?php echo bloginfo('template_directory')."/images/formations/dev-web.png"?> class="d-block w-100" alt="..."></a>
     </div>
     <div class="carousel-item">
-      <a href=""><img src=<?php echo bloginfo('template_directory')."/images/telecom.png"?> class="d-block w-100" alt="..."></a>
+      <a href=""><img src=<?php echo bloginfo('template_directory')."/images/formations/telecom.png"?> class="d-block w-100" alt="..."></a>
     </div>
     <div class="carousel-item">
-      <a href=""><img src= <?php echo bloginfo('template_directory')."/images/pixcertif.jpg"?> class="d-block w-100" alt="..."></a>
+      <a href=""><img src= <?php echo bloginfo('template_directory')."/images/formations/pixcertif.jpg"?> class="d-block w-100" alt="..."></a>
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
@@ -52,42 +52,55 @@ include('header.php')
 
 </section>
 <div class="button__formations mt-5 mb-5">
-<a class="d-flex justify-content-center" href=""><button class="btn btn-warning" type="button">Voir les autres formations</button></a>
+<a class="d-flex justify-content-center" href=""><button class="btn" type="button">Voir les autres formations</button></a>
 </div>
 <!-- Section cards -->
-<section>
+<section class="actu">
+  <h2 class="actu__title text-center text-uppercase mb-4 mt-4">Les news du greta</h2>
 <div class="row row-cols-1 row-cols-md-3 g-4">
   <div class="col">
-    <div class="card h-100">
-      <img src="<?php echo bloginfo('template_directory')."/images/theatre.jpg"?>" class="card-img-top" alt="...">
-      <div class="card-body">
+    <div class="card h-100 ">
+      <img src="<?php echo bloginfo('template_directory')."/images/actualités/theatre.jpg"?>" class="card-img-top" alt="...">
+      <div class="card-body d-flex flex-column justify-content-between">
         <h5 class="card-title">Saison culturelle 23-24 de la ville de Mende</h5>
         <p class="card-text">Rendez-vous est donné le 15octobre pour un lancement de saison époustouflant avec la compagnie Basinga, qui investira le quartier de Fontanilles pour y tendre un fil à travers ciel, et offrira au public lozérien une traversée funambule participative à couper le souffle.</p>
-        <a class="d-flex justify-content-center" href=""><button class="btn btn-primary" type="button">CONTACT</button></a>
+        <a class="d-flex justify-content-center" href=""><button class="btn" type="button">En savoir plus</button></a>
       </div>      
     </div>
   </div>
   <div class="col">
     <div class="card h-100">
-      <img src="<?php echo bloginfo('template_directory')."/images/étudiants.jpeg"?>" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Journée des étudiants : 2e édition</h5>
-        <p class="card-text">C’est donc le 7 septembre prochain, sous le slogan « Mets tes baskets et prépare tes neurones, ça va bouger ! » que près de 1 000 étudiants mendois sont conviés à une après-midi de rentrée.</p>
-        <a class="d-flex justify-content-center" href=""><button class="btn btn-primary" type="button">CONTACT</button></a>
+      <img src="<?php echo bloginfo('template_directory')."/images/actualités/actutelecom.jpg"?>" class="card-img-top" alt="...">
+      <div class="card-body d-flex flex-column justify-content-between">
+        <h5 class="card-title">La formation Tech’n camp s’installe dans le paysage numérique lozérien !</h5>
+        <p class="card-text">En Septembre 2020, Le GRETA-CFA de Lozère imagine une nouvelle formation intitulée « Techn’camp » qui vise à former des informaticiens spécialistes des systèmes et réseaux. Celle-ci s’inspire beaucoup du format, intensif et ouvert, développé par « Codi n’camp », une formation de programmeur web imaginée en Lozère et qui constituait à l’époque une petite révolution dans le champ des formations supérieures.</p>
+        <a class="d-flex justify-content-center" href=""><button class="btn" type="button">En savoir plus</button></a>
       </div>      
     </div>
   </div>
   <div class="col">
     <div class="card h-100">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-        <a class="d-flex justify-content-center" href=""><button class="btn btn-primary" type="button">CONTACT</button></a>
+      <img src="<?php echo bloginfo('template_directory')."/images/actualités/actudev.jpg"?>" class="card-img-top" alt="...">
+      <div class="card-body d-flex flex-column justify-content-between">
+        <h5 class="card-title">DÉVELOPPEUR WEB ET WEB MOBILE</h5>
+        <p class="card-text">- Développer ou faire évoluer des applications orientées web ou web mobile<br>
+        - Concevoir la partie visuelle de l'application exécutée sur le navigateur côté ordinateur client<br>
+        - Construire la maquette des écrans de l'interface utilisateur et prévoir la navigation de l'application web ou web mobile<br>
+        - Coder l'ensemble avec les langages de programmation web adaptés et une architecture applicative conforme aux bonnes pratiques du développement web et web mobile<br>
+        - Concevoir et coder la partie back-end<br>
+        </p>
+        <a class="d-flex justify-content-center" href=""><button class="btn" type="button">En savoir plus</button></a>
       </div>
     </div>
   </div>
 </div>
+</section>
+<section class="partner d-flex justify-content-between">
+  <a href=""><img src="<?php echo bloginfo('template_directory')."/images/partner/greta.jpg"?>" alt=""></a>
+  <a href=""><img src="<?php echo bloginfo('template_directory')."/images/partner/lozeredep.jpg"?>" alt=""></a>
+  <a href=""><img src="<?php echo bloginfo('template_directory')."/images/partner/lozeredev.png"?>" alt=""></a>
+  <a href=""><img src="<?php echo bloginfo('template_directory')."/images/partner/numerique.jpg"?>" alt=""></a>
+  <a href=""><img src="<?php echo bloginfo('template_directory')."/ressources/polen.jpg"?>" alt=""></a>
 </section>
 
 
