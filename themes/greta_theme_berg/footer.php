7924d2283cd43f5ec4bbac447c25b71afdcd2670
vincent48dev <147699347+vincent48dev@users.noreply.github.com>
2024-02-15 13:43:55 +0100

    maj

--- themes/greta_theme_berg/footer.php
+++ themes/greta_theme_berg/footer.php
@@ -1,13 +1,23 @@
 
-<footer class="bg-body-tertiary text-center text-lg-start">
+<footer class=" pt-3 bg-body-tertiary text-center text-lg-start">
   <!-- Copyright -->
 
-    <div class="text-center p-3"">
+    
+
+    <div class="buttonres d-flex justify-content-center">
+
+        <a class="d-flex me-3" href="https://twitter.com/DRAFPICA_Occ"target="_blank"><i class="bi bi-twitter-x"></i></a>
+        <a class="d-flex ms-3" href="https://www.linkedin.com/pulse/le-greta-cfa-gard-loz%C3%A8re-lab%C3%A9lis%C3%A9-greta-occitanie"target="_blank"><i class="bi bi-linkedin"></i></a>
+    </div>
+
+    <div class="text-center p-3">
         © 2024 Copyright By Greta Von Teese
     </div>
     <!-- Copyright -->
     
     <div class="fr-col-12 fr-col-sm-6 fr-col-md-3 text-center">
+        
+
         <ul>
             <li>
                 <a href="<?php echo bloginfo('url')."/mentions-legales"?>" target="_blank">Mentions légales</a>
