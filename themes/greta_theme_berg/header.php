ed7115cf9bb6b51ba8376ede54bb9000fc7b2adb
Floconi <kilian.dwwn48@gmail.com>
2024-02-19 11:18:39 +0100

    Mettre le menu en dynamique

--- themes/greta_theme_berg/header.php
+++ themes/greta_theme_berg/header.php
@@ -27,8 +27,8 @@
       <img class=" w-25 ms-4" src="<?php echo bloginfo('template_directory')."/images/logo.png"?>" alt="occitanie">
     </div>
     <div class="col-sm w-50 d-flex  justify-content-end">
-            <nav class=" navbar navbar-expand-lg">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <nav class=" navbar navbar-expand-lg d-flex  justify-content-end">
+            <button class="navbar-toggler  " type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse menu-principal" id="navbarNav">
